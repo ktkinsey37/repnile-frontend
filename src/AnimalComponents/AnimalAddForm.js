bdf3132a040cd1ce18e076a2bb2f9fe8e1211490
halim.tannous <halim.tannous@gmail.com>
2022-02-01 00:03:30 +0100

    session

--- src/AnimalComponents/AnimalAddForm.js
+++ src/AnimalComponents/AnimalAddForm.js
@@ -16,6 +16,7 @@ import RepnileApi from "../api";
 
 function AddAnimalForm() {
   const navigate = useNavigate();
+  const [file, setFile] = useState("");
   const [formData, setFormData] = useState({
     name: "",
     species: "",
@@ -26,15 +27,17 @@ function AddAnimalForm() {
     primaryColor: "",
     secondaryColor: "",
     price: "",
-    forSale: true
+    forSale: true,
   });
   const [formErrors, setFormErrors] = useState([]);
   // const [forSale, setForSale] = useState(true)
 
   console.debug(
-      "AddAnimalForm",
-      "formData=", formData,
-      "formErrors=", formErrors,
+    "AddAnimalForm",
+    "formData=",
+    formData,
+    "formErrors=",
+    formErrors
   );
 
   /** Handle form submit:
@@ -42,10 +45,20 @@ function AddAnimalForm() {
    * Calls login func prop and, if successful, redirect to /companies.
    */
 
+  const handleOnUploadFile = (e) => {
+    setFile(e.target.files[0]);
+  };
+
   async function handleSubmit(evt) {
     evt.preventDefault();
-    let result = await RepnileApi.addAnimal(formData);
-    console.log(result, "this is result in handlesubmit of addanimalform")
+    const newFormData = new FormData();
+    Object.entries(formData).forEach(([k, v]) => {
+      newFormData.append(k, v);
+    });
+    console.log(newFormData);
+    newFormData.append("imgUrl", file);
+    let result = await RepnileApi.addAnimal(newFormData);
+    console.log(result, "this is result in handlesubmit of addanimalform");
     if (result) {
       navigate("/animals");
     } else {
@@ -56,151 +69,150 @@ function AddAnimalForm() {
   /** Update form data field */
   function handleChange(evt) {
     let { name, value } = evt.target;
-    console.log(evt.target, "this is evt target")
-    if (evt.target.type == "checkbox"){
-      evt.target.checked = !evt.target.checked
+    console.log(evt.target, "this is evt target");
+    if (evt.target.type == "checkbox") {
+      evt.target.checked = !evt.target.checked;
 
-      value =  !evt.target.checked
-      console.log(evt.target.checked, "this is evt.tgt.check")
-      console.log(value, "this is value in hitting the check")
+      value = !evt.target.checked;
+      console.log(evt.target.checked, "this is evt.tgt.check");
+      console.log(value, "this is value in hitting the check");
     }
-    setFormData(data => ({ ...data, [name]: value }));
+    setFormData((data) => ({ ...data, [name]: value }));
   }
 
   return (
-      <div className="AddAnimalForm">
-        <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-          <h2 className="mb-3">Sign Up</h2>
-          <div className="card">
-            <div className="card-body">
-              <form onSubmit={handleSubmit}>
+    <div className="AddAnimalForm">
+      <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+        <h2 className="mb-3">Sign Up</h2>
+        <div className="card">
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label>Name</label>
+                <input
+                  name="name"
+                  className="form-control"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </div>
 
-                <div className="form-group">
-                  <label>Name</label>
-                  <input
-                      name="name"
-                      className="form-control"
-                      value={formData.name}
-                      onChange={handleChange}
-                  />
-                </div>
-
-                <div className="form-group">
+              <div className="form-group">
                 <label>Species</label>
                 <input
-                    name="species"
-                    className="form-control"
-                    value={formData.species}
-                    onChange={handleChange}
+                  name="species"
+                  className="form-control"
+                  value={formData.species}
+                  onChange={handleChange}
                 />
-                </div>
+              </div>
 
-                <div className="form-group">
-                  <label>Birth Date</label>
-                  <input
-                      name="birthDate"
-                      className="form-control"
-                      value={formData.birthDate}
-                      onChange={handleChange}
-                  />
-                </div>
-                
-                <div className="form-group">
-                  <label>Sex</label>
-                  <input
-                      name="sex"
-                      className="form-control"
-                      value={formData.sex}
-                      onChange={handleChange}
-                  />
-                </div>
+              <div className="form-group">
+                <label>Birth Date</label>
+                <input
+                  name="birthDate"
+                  className="form-control"
+                  value={formData.birthDate}
+                  onChange={handleChange}
+                />
+              </div>
 
-                <div className="form-group">
-                  <label>Weight</label>
-                  <input
-                      name="weight"
-                      className="form-control"
-                      value={formData.weight}
-                      onChange={handleChange}
-                  />
-                </div>
+              <div className="form-group">
+                <label>Sex</label>
+                <input
+                  name="sex"
+                  className="form-control"
+                  value={formData.sex}
+                  onChange={handleChange}
+                />
+              </div>
 
-                <div className="form-group">
+              <div className="form-group">
+                <label>Weight</label>
+                <input
+                  name="weight"
+                  className="form-control"
+                  value={formData.weight}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="form-group">
                 <label>Coloration Pattern</label>
                 <input
-                    name="colorationPattern"
-                    className="form-control"
-                    value={formData.colorationPattern}
-                    onChange={handleChange}
+                  name="colorationPattern"
+                  className="form-control"
+                  value={formData.colorationPattern}
+                  onChange={handleChange}
                 />
-                </div>
+              </div>
 
-                <div className="form-group">
+              <div className="form-group">
                 <label>Primary Color</label>
                 <input
-                    name="primaryColor"
-                    className="form-control"
-                    value={formData.primaryColor}
-                    onChange={handleChange}
+                  name="primaryColor"
+                  className="form-control"
+                  value={formData.primaryColor}
+                  onChange={handleChange}
                 />
-                </div>
+              </div>
 
-                <div className="form-group">
+              <div className="form-group">
                 <label>Secondary Color</label>
                 <input
-                    name="secondaryColor"
-                    className="form-control"
-                    value={formData.secondaryColor}
-                    onChange={handleChange}
+                  name="secondaryColor"
+                  className="form-control"
+                  value={formData.secondaryColor}
+                  onChange={handleChange}
                 />
-                </div>
+              </div>
 
-                <div className="form-group">
+              <div className="form-group">
                 <label>Price</label>
                 <input
-                    name="price"
-                    className="form-control"
-                    value={formData.price}
-                    onChange={handleChange}
+                  name="price"
+                  className="form-control"
+                  value={formData.price}
+                  onChange={handleChange}
                 />
-                </div>
+              </div>
 
-                <div className="form-group">
-                <label>For Sale
-                <input
+              <div className="form-group">
+                <label>
+                  For Sale
+                  <input
                     name="forSale"
                     type="checkbox"
                     checked={formData.forSale}
                     onChange={handleChange}
-                /></label>
-                </div>
+                  />
+                </label>
+              </div>
 
-                <div className="form-group">
+              <div className="form-group">
                 <label>Picture</label>
                 <input
-                    type="file"
-                    name="imgUrl"
-                    accept="image/png, image/jpeg"
-                    className="form-control"
-                    value={formData.imgUrl}
-                    onChange={handleChange}
+                  type="file"
+                  name="imgUrl"
+                  accept="image/png, image/jpeg"
+                  className="form-control"
+                  value={formData.imgUrl}
+                  onChange={handleOnUploadFile}
                 />
-                </div>
+              </div>
 
-
-
-                <button
-                    type="submit"
-                    className="btn btn-primary float-right"
-                    onSubmit={handleSubmit}
-                >
-                  Submit
-                </button>
-              </form>
-            </div>
+              <button
+                type="submit"
+                className="btn btn-primary float-right"
+                onSubmit={handleSubmit}
+              >
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
