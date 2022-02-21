0b82baa98ffc2879463642f78087109890110241
halim.tannous <halim.tannous@gmail.com>
2022-02-22 00:10:35 +0100

    post session

--- src/AnimalComponents/Animal.js
+++ src/AnimalComponents/Animal.js
@@ -1,83 +1,78 @@
-import React, {useEffect, useState, useContext} from "react";
-import {
-    Card,
-    CardBody,
-    CardTitle,
-    CardText,
-  } from "reactstrap";
+import React, { useEffect, useState, useContext } from "react";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
 import RepnileApi from "../api";
 import AnimalCard from "./AnimalCard";
 import { Redirect, useParams } from "react-router-dom";
 import UserContext from "../UserContext";
 import { Link } from "react-router-dom";
 
-
-
-
 function Animal() {
-    const { id } = useParams();
-    const [isLoading, setIsLoading] = useState(true);
-    const [animal, setAnimal] = useState([]);
-    const user = useContext(UserContext)
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [animal, setAnimal] = useState([]);
+  const user = useContext(UserContext);
 
-  
-    useEffect(function getAnimalOnLoad() {
+  useEffect(
+    function getAnimalOnLoad() {
       async function getAnimal() {
         let animal = await RepnileApi.getAnimal(id);
         // should i do parents here?
-        console.log(animal, "this is animal in  getanimalonload")
-        setAnimal(animal)
+        console.log(animal, "this is animal in  getanimalonload");
+        setAnimal(animal);
         setIsLoading(false);
       }
-      getAnimal()
-    }, [id]);
+      getAnimal();
+    },
+    [id]
+  );
 
-    const forSale = animal.forSale ? "This critter is for sale!" : "This critter is currently unavailable"
-  
-    if (isLoading) {
-      return <p>Loading &hellip;</p>;
-    }
-  
-    return (
-      <section className="col-md-4">
-        <Card>
-          <CardBody>
-            <CardTitle className="font-weight-bold text-center">
-              {animal.name}
-              <br/>
-              {forSale}
-              <br/>
-              Weight: {animal.weight} grams
-              <br/>
-              Coloration Pattern: {animal.colorationPattern}
-              <br/>
-              Primary Color: {animal.primaryColor}
-              <br/>
-              Secondary Color: {animal.secondaryColor}
-              <br/>
-              Birth Date: {animal.birth_date}
-              <br/>
-              Price: ${animal.price}
-              <br/>
-              Sex: {animal.sex}
-              <br/>
-              Species: {animal.species}
-              <br/>
-              <img src={animal.imgUrl} alt={animal.name} width="500" height="600"/>
-              <br/>
+  const forSale = animal.forSale
+    ? "This critter is for sale!"
+    : "This critter is currently unavailable";
 
-              <Link to={`/animals/${animal.id}/edit`}>Edit
-              </Link>
-
-              
-            </CardTitle>
-            <CardText>
-
-            </CardText>
-          </CardBody>
-        </Card>
-      </section>
-    );
+  if (isLoading) {
+    return <p>Loading &hellip;</p>;
   }
-  
-  export default Animal;
+
+  return (
+    <section className="col-md-4">
+      <Card>
+        <CardBody>
+          <CardTitle className="font-weight-bold text-center">
+            {animal.name}
+            <br />
+            {forSale}
+            <br />
+            Weight: {animal.weight} grams
+            <br />
+            Coloration Pattern: {animal.colorationPattern}
+            <br />
+            Primary Color: {animal.primaryColor}
+            <br />
+            Secondary Color: {animal.secondaryColor}
+            <br />
+            Birth Date: {animal.birth_date}
+            <br />
+            Price: ${animal.price}
+            <br />
+            Sex: {animal.sex}
+            <br />
+            Species: {animal.species}
+            <br />
+            <img
+              src={RepnileApi.getImage(animal.imgUrl)}
+              alt={animal.name}
+              width="500"
+              height="600"
+            />
+            <br />
+            <Link to={`/animals/${animal.id}/edit`}>Edit</Link>
+          </CardTitle>
+          <CardText></CardText>
+        </CardBody>
+      </Card>
+    </section>
+  );
+}
+
+export default Animal;
--- src/ItemComponents/ItemsPage.js
+++ src/ItemComponents/ItemsPage.js
@@ -8,77 +8,55 @@ import {
   ListGroup,
   ListGroupItem,
 } from "reactstrap";
-import RepnileApi from "../api"
+import RepnileApi from "../api";
+
+// function ItemsPage({}) {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [animals, setAnimals] = useState([]);
+
+//   useEffect(() => {
+//     async function getAnimals() {
+//       let animals = await RepnileApi.getAllAnimals();
+//       console.log(typeof animals, "animals in the useeffect");
+//       setAnimals(animals);
+//       setIsLoading(false);
+//     }
+//     getAnimals();
+//   }, []);
 
 function ItemsPage({}) {
   const [isLoading, setIsLoading] = useState(true);
-  const [animals, setAnimals] = useState([]);
+  const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    async function getAnimals() {
-      let animals = await RepnileApi.getAllAnimals();
-      console.log(typeof animals, "animals in the useeffect");
-      setAnimals(animals);
-      setIsLoading(false);
-    }
-    getAnimals();
-  }, []);
+  // useEffect(() => {
+  //   async function getItems() {
+  //     let items = await RepnileApi.getAllItems();
+  //     console.log(typeof items, "animals in the useeffect");
+  //     setAnimals(items);
+  //     setIsLoading(false);
+  //   }
+  //   getItems();
+  // }, []);
 
-function ItemsPage({ }) {
+  // async function search(params) {
+  //   setIsLoading(true);
+  //   console.log(params, "params in search isnide animalspage");
+  //   let items = await RepnileApi.getItemsQuery(params);
+  //   setItems(items);
+  //   setIsLoading(false);
+  // }
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [items, setItems] = useState([]);
+  // if (isLoading) {
+  //   return <p>Loading &hellip;</p>;
+  // }
 
-    useEffect(() => {
-      async function getItems() {
-        let items = await RepnileApi.getAllItems();
-        console.log(typeof(items), "animals in the useeffect")
-        setAnimals(items)
-        setIsLoading(false);
-      }
-      getItems()
-    }, []);
+  // console.log(animals, "animals before the map");
+  // console.log(
+  //   animals.map((animal) => console.log(animal)),
+  //   "this is animals on animals page"
+  // );
 
-    async function search(params) {
-        setIsLoading(true)
-        console.log(params, "params in search isnide animalspage")
-        let items = await RepnileApi.getItemsQuery(params);
-        setItems(items);
-        setIsLoading(false);
-      }
-  
-    if (isLoading) {
-      return <p>Loading &hellip;</p>;
-    }
-
-    console.log(animals, "animals before the map")
-    console.log(animals.map((animal) => console.log(animal)), "this is animals on animals page")
-
-  return (
-    <div className="col-md-12 offset-md-4">
-      <br />
-      <section className="col-md-10">
-        <Card style={{ width: "30rem" }}>
-          <CardBody className="col-md-12 ">
-            <CardTitle className="font-weight-bold text-center">
-              <h2>Animals</h2>
-            </CardTitle>
-            <CardText>
-              <AnimalSearchForm search={search} />
-            </CardText>
-
-            <Card>
-              {animals.map((animal) => (
-                <Link to={`/animals/${animal.id}`}>
-                  <AnimalCard animal={animal} />
-                </Link>
-              ))}
-            </Card>
-          </CardBody>
-        </Card>
-      </section>
-    </div>
-  );
+  return <div> </div>;
 }
 
 // The above can go inside of list group
